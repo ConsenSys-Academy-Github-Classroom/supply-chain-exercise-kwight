152315910d78b3fc0383c4517c988c5ec423cdb0
Kirk Wight <kwight@kwight.ca>
2021-10-02 12:19:02 -0700

    Add an `isOwner` modifier according to instructions (although it's not needed to pass any tests).

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -44,7 +44,10 @@ contract SupplyChain {
 
   // Create a modifer, `isOwner` that checks if the msg.sender is the owner of the contract
 
-  // <modifier: isOwner
+  modifier isOwner() {
+    require(msg.sender == owner, "Unauthorized.");
+    _;
+  }
 
   modifier verifyCaller (address _address) { 
     // require (msg.sender == _address); 
